24bc093d70d7a8b7bcf8b947195255fa5c2cfba8
wawandx <rachmadkurniawandev@gmail.com>
2021-07-29 22:51:49 +0800

    feat: create section categories

---
+++ src/parts/Categories.js
@@ -0,0 +1,61 @@
+import React from "react";
+
+import Button from "components/Button";
+
+const Categories = ({ data }) => {
+  return data.map((category, indexCategory) => {
+    return (
+      <section className="container" key={`category-${indexCategory}`}>
+        <h4 className="mb-3 font-weight-medium">{category.name}</h4>
+        <div className="container-grid">
+          {category.items.length === 0 ? (
+            <div className="row">
+              <div className="col-auto align-items-center">
+                There is no property at this category
+              </div>
+            </div>
+          ) : (
+            category.items.map((item, indexItem) => {
+              return (
+                <div
+                  className="item column-3 row-1"
+                  key={`category-${indexCategory}-item-${indexItem}`}
+                >
+                  <div className="card">
+                    {item.isPopular && (
+                      <div className="tag">
+                        Popular{" "}
+                        <span className="font-weight-light">Choice</span>
+                      </div>
+                    )}
+                    <figure className="img-wrapper" style={{ height: 180 }}>
+                      <img
+                        src={item.imageUrl}
+                        alt={item.name}
+                        className="img-cover"
+                      />
+                    </figure>
+                    <div className="meta-wrapper">
+                      <Button
+                        className="streched-link d-block text-gray-900"
+                        type="link"
+                        href={`/properties/${item._id}`}
+                      >
+                        <h5 className="h4">{item.name}</h5>
+                      </Button>
+                      <span className="text-gray-500">
+                        {`${item.city}, ${item.country}`}
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              );
+            })
+          )}
+        </div>
+      </section>
+    );
+  });
+};
+
+export default Categories;
--- src/screens/LandingPage.js
+++ src/screens/LandingPage.js
@@ -3,6 +3,7 @@ import React, { useRef } from "react";
 import Header from "parts/Header";
 import Hero from "parts/Hero";
 import MostPicked from "parts/MostPicked";
+import Categories from "parts/Categories";
 
 import landingPage from "json/landingPage.json";
 
@@ -13,6 +14,7 @@ const LandingPage = (props) => {
       <Header {...props} />
       <Hero refMostPicked={refMostPicked} data={landingPage.hero} />
       <MostPicked refMostPicked={refMostPicked} data={landingPage.mostPicked} />
+      <Categories data={landingPage.categories} />
     </>
   );
 };
