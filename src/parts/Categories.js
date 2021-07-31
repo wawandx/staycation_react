071c48e8259f448e62158f84b9182a4e6a854233
wawandx <rachmadkurniawandev@gmail.com>
2021-07-31 12:27:26 +0800

    chore: animate landing page

--- src/parts/Categories.js
+++ src/parts/Categories.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Fade from "react-reveal/Fade";
 
 import Button from "components/Button";
 
@@ -6,53 +7,57 @@ const Categories = ({ data }) => {
   return data.map((category, indexCategory) => {
     return (
       <section className="container" key={`category-${indexCategory}`}>
-        <h4 className="mb-3 font-weight-medium">{category.name}</h4>
-        <div className="container-grid">
-          {category.items.length === 0 ? (
-            <div className="row">
-              <div className="col-auto align-items-center">
-                There is no property at this category
-              </div>
-            </div>
-          ) : (
-            category.items.map((item, indexItem) => {
-              return (
-                <div
-                  className="item column-3 row-1"
-                  key={`category-${indexCategory}-item-${indexItem}`}
-                >
-                  <div className="card">
-                    {item.isPopular && (
-                      <div className="tag">
-                        Popular{" "}
-                        <span className="font-weight-light">Choice</span>
-                      </div>
-                    )}
-                    <figure className="img-wrapper" style={{ height: 157 }}>
-                      <img
-                        src={item.imageUrl}
-                        alt={item.name}
-                        className="img-cover"
-                      />
-                    </figure>
-                    <div className="meta-wrapper">
-                      <Button
-                        className="streched-link d-block text-gray-900"
-                        type="link"
-                        href={`/properties/${item._id}`}
-                      >
-                        <h5 className="h4">{item.name}</h5>
-                      </Button>
-                      <span className="text-gray-500">
-                        {`${item.city}, ${item.country}`}
-                      </span>
-                    </div>
-                  </div>
+        <Fade bottom>
+          <h4 className="mb-3 font-weight-medium">{category.name}</h4>
+          <div className="container-grid">
+            {category.items.length === 0 ? (
+              <div className="row">
+                <div className="col-auto align-items-center">
+                  There is no property at this category
                 </div>
-              );
-            })
-          )}
-        </div>
+              </div>
+            ) : (
+              category.items.map((item, indexItem) => {
+                return (
+                  <div
+                    className="item column-3 row-1"
+                    key={`category-${indexCategory}-item-${indexItem}`}
+                  >
+                    <Fade bottom delay={300 * indexItem}>
+                      <div className="card">
+                        {item.isPopular && (
+                          <div className="tag">
+                            Popular{" "}
+                            <span className="font-weight-light">Choice</span>
+                          </div>
+                        )}
+                        <figure className="img-wrapper" style={{ height: 157 }}>
+                          <img
+                            src={item.imageUrl}
+                            alt={item.name}
+                            className="img-cover"
+                          />
+                        </figure>
+                        <div className="meta-wrapper">
+                          <Button
+                            className="streched-link d-block text-gray-900"
+                            type="link"
+                            href={`/properties/${item._id}`}
+                          >
+                            <h5 className="h4">{item.name}</h5>
+                          </Button>
+                          <span className="text-gray-500">
+                            {`${item.city}, ${item.country}`}
+                          </span>
+                        </div>
+                      </div>
+                    </Fade>
+                  </div>
+                );
+              })
+            )}
+          </div>
+        </Fade>
       </section>
     );
   });
--- src/parts/Testimonial.js
+++ src/parts/Testimonial.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Fade from "react-reveal/Fade";
 
 import testimonialFrame from "assets/images/hero/image-testimonial-frame.png";
 
@@ -7,53 +8,58 @@ import Button from "components/Button";
 
 const Testimonial = ({ data }) => {
   return (
-    <section className="container">
-      <div className="row align-items-center">
-        <div className="col-md-5">
-          <div className="testimonial-hero" style={{ margin: `30px 0 0 30px` }}>
-            <img
-              src={data.imageUrl}
-              alt="testimonial"
-              className="position-absolute"
-              style={{
-                zIndex: 1,
-              }}
-            />
-            <img
-              src={testimonialFrame}
-              alt="testimonial frame"
-              className="position-absolute"
-              style={{
-                margin: `-30px 0 0 -30px`,
-              }}
-            />
-          </div>
-        </div>
-        <div className="col-md-7">
-          <h4 style={{ marginBottom: 40 }}>{data.name}</h4>
-          <Star value={data.rate} width={35} height={35} spacing={4}></Star>
-          <h5
-            className="h2 font-weight-normal line-height-2"
-            style={{ marginTop: 20 }}
-          >
-            {data.content}
-          </h5>
-          <span className="text-gray-500">{`${data.familyName}, ${data.familyOccupation}`}</span>
-          <div>
-            <Button
-              className="btn px-5"
-              style={{ marginTop: 40 }}
-              hasShadow
-              isPrimary
-              type="link"
-              href={`/testimonial/${data._id}`}
+    <Fade bottom>
+      <section className="container">
+        <div className="row align-items-center">
+          <div className="col-md-5">
+            <div
+              className="testimonial-hero"
+              style={{ margin: `30px 0 0 30px` }}
             >
-              Read Their Story
-            </Button>
+              <img
+                src={data.imageUrl}
+                alt="testimonial"
+                className="position-absolute"
+                style={{
+                  zIndex: 1,
+                }}
+              />
+              <img
+                src={testimonialFrame}
+                alt="testimonial frame"
+                className="position-absolute"
+                style={{
+                  margin: `-30px 0 0 -30px`,
+                }}
+              />
+            </div>
+          </div>
+          <div className="col-md-7">
+            <h4 style={{ marginBottom: 40 }}>{data.name}</h4>
+            <Star value={data.rate} width={35} height={35} spacing={4}></Star>
+            <h5
+              className="h2 font-weight-normal line-height-2"
+              style={{ marginTop: 20 }}
+            >
+              {data.content}
+            </h5>
+            <span className="text-gray-500">{`${data.familyName}, ${data.familyOccupation}`}</span>
+            <div>
+              <Button
+                className="btn px-5"
+                style={{ marginTop: 40 }}
+                hasShadow
+                isPrimary
+                type="link"
+                href={`/testimonial/${data._id}`}
+              >
+                Read Their Story
+              </Button>
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </Fade>
   );
 };
 
